b169fc401180911713bcc0e754f60e1336da58e0
dialgacylert <dcylert@gmail.com>
2023-05-08 23:26:18 +0700

    fix path

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import NotFound from '../views/NotFound.vue'
 import AboutView from '../views/AboutView.vue'
-import MyApp from '../views/AboutView.vue'
+import MyApp from '../views/MyApp.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),

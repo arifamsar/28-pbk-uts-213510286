f000adb24ca10c4b2098787838f9cae2126346c4
dialgacylert <muhammadarifamsar@gmail.com>
2023-05-08 20:26:31 +0700

    Add router features, etc

--- src/App.vue
+++ src/App.vue
@@ -1,68 +1,39 @@
 <script setup>
-import { ref, computed } from 'vue'
-import { Input, Button } from 'flowbite-vue'
-import { XCircleIcon } from "@heroicons/vue/24/outline";
+import { RouterLink, RouterView } from 'vue-router'
 
-
-let id = 0
-
-const newTodo = ref('')
-const hideCompleted = ref(true)
-const todos = ref([
-  { id: id++, text: 'Belajar PBK', done: true },
-  { id: id++, text: 'Belajar PBO', done: false }
-])
-
-const filteredTodos = computed(() => {
-  return hideCompleted.value
-    ? todos.value.filter((t) => !t.done)
-    : todos.value
-})
-
-function addTodo() {
-  todos.value.push({ id: id++, text: newTodo.value, done: false })
-  newTodo.value = ''
-}
-
-function removeTodo(todo) {
-  todos.value = todos.value.filter((t) => t !== todo)
-}
 </script>
 
 <template>
-  <div class="container mx-auto">
-    <div class="justify-center py-10 bg-blue-100 rounded-xl mt-6 mx-6">
-      <h1 class="uppercase font-bold text-3xl text-center pb-4">List Kegiatan</h1>
-      <form @submit.prevent="addTodo">
-        <div class="mb-6 px-10">
-          <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Tambah Kegiatan</label>
-          <input v-model="newTodo" type="text" placeholder="Tambahkan kegiatan..."
-            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-        </div>
-        <button type="submit"
-          class="mx-10 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm  px-5 py-2.5 text-center">Submit</button>
-      </form>
-      <div class="relative overflow-x-auto sm:rounded-lg mx-10 my-10">
-        <ul class="list-none flex-row mx-auto">
-          <li v-for="todo in filteredTodos" :key="todo.id" class="p-2 font-medium text-gray-900">
-            <div class="flex items-center justify-center bg-blue-200 rounded py-2">
-              <span :class="{ done: todo.done}">{{ todo.text }}</span>
-              <input class="ml-2 w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2 " type="checkbox" v-model="todo.done">
-              <button class="ml-2" @click="removeTodo(todo)">
-                <XCircleIcon class="h-6 w-6 text-red-400 hover:text-red-700" />
-              </button>
-            </div>
-          </li>
-        </ul>
-      </div>
-      <button class="mx-10 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center" @click="hideCompleted = !hideCompleted">{{ hideCompleted ? 'Tampilkan Semua' : 'Tampilkan yang belum selesai' }}</button>
-    </div>
-  </div>
+  <header>
+      <nav>
+        <RouterLink to="/">Home</RouterLink> |
+        <RouterLink to="/about">About</RouterLink> |
+        <RouterLink to="/myapp">MyApp</RouterLink>
+      </nav>
+  </header>
+
+  <RouterView />
 </template>
 
-<style scoped>
-.done {
-  text-decoration: line-through;
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
 }
 
+nav {
+  padding: 30px;
+  text-align: center;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
 
-import './index.css'
-createApp(App).mount('#app')
+import './assets/index.css'
+
+const app = createApp(App)
+
+app.use(router)
+
+app.mount('#app')
 
 export default {
 	components: {
